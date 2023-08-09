7193a964ebfd0cfd8c7472518d8ec1c78f00c90a
Themanh99 <themanhchu99@gmail.com>
2023-08-09 19:20:16 +0700

    fix js run app

--- js/app.js
+++ js/app.js
@@ -12,9 +12,7 @@ function addNav() {
     let li = document.createElement("li");
     li.classList.add("item");
     li.id = `item${i}`;
-    li.innerHTML = `<a href="" id="#section${i}">${item[i - 1].getAttribute(
-      "data-nav"
-    )}</a>`;
+    li.innerHTML = `<a href="" id="#section${i}">${item[i - 1].getAttribute("data-nav")}</a>`;
     ul.appendChild(li);
   }
   nav.append(ul);
@@ -24,43 +22,40 @@ function addNav() {
 addNav();
 // scroll to anchor
 
-const nav = document.querySelectorAll("nav a");
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelectorAll("nav a");
 
-// Add click event listeners to navigation links
-nav.forEach((link) => {
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
-    const targetSectionId = link.getAttribute("id");
-    const targetSection = document.querySelector(targetSectionId);
+  // Add click event listeners to navigation links
+  nav.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const targetSectionId = link.getAttribute("id");
+      const targetSection = document.querySelector(targetSectionId);
 
-    // Scroll to the appropriate section smoothly
-    targetSection.scrollIntoView({ behavior: "smooth", block: "center" });
+      // Scroll to the appropriate section smoothly
+      targetSection.scrollIntoView({ behavior: "smooth", block: "center" });
 
-    // Update active state for the clicked link
-    nav.forEach((li) => li.classList.remove("active"));
-    link.classList.add("active");
-  });
-});
-
-// Update active state based on current scroll position
-window.addEventListener("scroll", function () {
-  const sections = document.querySelectorAll("section");
-  const scrollPosition = window.scrollY + 100;
-  sections.forEach((section) => {
-    section.classList.remove("active");
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.offsetHeight;
-    const sectionId = section.getAttribute("id");
-    if (
-      scrollPosition >= sectionTop &&
-      scrollPosition < sectionTop + sectionHeight
-    ) {
+      // Update active state for the clicked link
       nav.forEach((li) => li.classList.remove("active"));
+      link.classList.add("active");
+    });
+  });
 
-      document
-        .querySelector(`nav a[id="#${sectionId}"]`)
-        .classList.add("active");
-      section.classList.add("active");
-    }
+  // Update active state based on current scroll position
+  window.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll("section");
+    const scrollPosition = window.scrollY + 100;
+    sections.forEach((section) => {
+      section.classList.remove("active");
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionId = section.getAttribute("id");
+      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+        nav.forEach((li) => li.classList.remove("active"));
+
+        document.querySelector(`nav a[id="#${sectionId}"]`).classList.add("active");
+        section.classList.add("active");
+      }
+    });
   });
 });
